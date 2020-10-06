3e8f16b458ed9e1ff5962152039d45f62b335cfc
Santkey <70204224+Santkey@users.noreply.github.com>
2020-10-06 15:24:00 +0200

    Add files via upload

---
+++ Ex7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int a = 5, b = 5;
+
+ if (a > b)
+{
+    cout << " A is lesser than B" << endl;
+}
+ else if (a < b)
+{
+    cout << " A is greater than B" << endl;
+}else
+{
+    cout << " A is equal to B" << endl;
+}
+
+    return 0;
+}
